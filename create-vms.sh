2530ea1fcb2e1d11ed6019b4fc8829827a9f5ac3
reza <mkeshtk@emory.edu>
2018-09-07 13:08:17 -0400

    updated create-vm

--- create-vms.sh
+++ create-vms.sh
@@ -4,16 +4,25 @@ SERVER_NAME=$1
 NUM_TPU=($2)
 BUCKET_NAME=$3
 ZONE=$4
-
 CODE_BUCKET='pbt-test-bucket-2'
-
 TF_VERSION='nightly'
 
-gcloud config set compute/zone ${ZONE}
-
-# create a bucket
-gsutil mb gs://${BUCKET_NAME}
-
+# create TPUs
+ind=1 &&
+tpu_node_counter=0 &&
+TPU_RANGE_LIST=$(gcloud beta compute tpus list --zone=${ZONE} --format='value(RANGE)') &&
+while [ $tpu_node_counter -lt $NUM_TPU ]
+do
+    ip_range=10.240.${ind}.0/29
+    if ! echo $TPU_RANGE_LIST | grep -q $ip_range; then
+        echo $ip_range
+        gcloud compute tpus create tpu-${SERVER_NAME}-${tpu_node_counter} --range ${ip_range} --zone ${ZONE} --version ${TF_VERSION} --preemptible --network default &
+        tpu_node_counter=$((tpu_node_counter+1))
+    fi
+    ind=$((ind+1))
+done &
+gcloud config set compute/zone ${ZONE} &&
+gsutil mb gs://${BUCKET_NAME} &&
 gcloud compute instances create ${SERVER_NAME} --machine-type=n1-standard-4 --boot-disk-size=200GB --image-project=ml-images --image-family=tf-${TF_VERSION} --zone=${ZONE} --scopes=cloud-platform  --tags=server-tag --metadata startup-script="#! /bin/bash
 gcloud config set compute/zone ${ZONE}
 apt-get install dirmngr
@@ -58,21 +67,9 @@ gcloud compute ssh ${SERVER_NAME} --zone=us-central1-f --command='sudo mongo adm
    authorization: enabled
 net: 
    bindIp: 127.0.0.1,`hostname -I`" | sudo tee -a /etc/mongod.conf && sudo systemctl restart mongod.service' &&
-echo "Mounting the bucket" ;
+echo "Mounting the bucket" &&
 gcloud compute ssh ${SERVER_NAME} --zone=us-central1-f --command="echo '${BUCKET_NAME} /${BUCKET_NAME} gcsfuse rw,auto,user' | sudo tee -a /etc/fstab
 mount /${BUCKET_NAME}
 mkdir /${BUCKET_NAME}/data
-mkdir /${BUCKET_NAME}/runs" ;
-ind=1 &&
-tpu_node_counter=0 &&
-TPU_RANGE_LIST=$(gcloud beta compute tpus list --zone=${ZONE} --format='value(RANGE)') &&
-while [ $tpu_node_counter -lt $NUM_TPU ]
-do
-    ip_range=10.240.${ind}.0/29
-    if ! echo $TPU_RANGE_LIST | grep -q $ip_range; then
-        echo $ip_range
-        gcloud compute tpus create tpu-${SERVER_NAME}-${tpu_node_counter} --range ${ip_range} --zone ${ZONE} --version ${TF_VERSION} --preemptible --network default &
-        tpu_node_counter=$((tpu_node_counter+1))
-    fi
-    ind=$((ind+1))
-done 
+mkdir /${BUCKET_NAME}/runs" 
+
