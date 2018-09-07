d6b62a11fa80a41867bd8cf2eff210a5744aa897
reza <mkeshtk@emory.edu>
2018-09-07 12:51:40 -0400

    updated create-vm

--- create-vms.sh
+++ create-vms.sh
@@ -2,19 +2,26 @@
 
 SERVER_NAME=$1
 NUM_TPU=($2)
-BUCKET_DIR=$3
+BUCKET_NAME=$3
 ZONE=$4
 
+CODE_BUCKET='pbt-test-bucket-2'
 
 TF_VERSION='nightly'
 
 gcloud config set compute/zone ${ZONE}
 
-gcloud compute instances create ${SERVER_NAME} --machine-type=n1-standard-4 --boot-disk-size=30GB --image-project=ml-images --image-family=tf-${TF_VERSION} --zone=${ZONE} --scopes=cloud-platform  --tags=server-tag --metadata startup-script="#! /bin/bash
+# create a bucket
+gsutil mb gs://${BUCKET_NAME}
+
+
+gcloud compute instances create ${SERVER_NAME} --machine-type=n1-standard-4 --boot-disk-size=200GB --image-project=ml-images --image-family=tf-${TF_VERSION} --zone=${ZONE} --scopes=cloud-platform  --tags=server-tag --metadata startup-script="#! /bin/bash
 gcloud config set compute/zone ${ZONE}
 apt-get install dirmngr
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 echo 'deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.0 main' | tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+echo 'deb http://packages.cloud.google.com/apt gcsfuse-stretch main' | sudo tee /etc/apt/sources.list.d/gcsfuse.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 apt-get update
 sudo apt-get install -y mongodb-org
 echo 'mongodb-org hold' | sudo dpkg --set-selections
@@ -25,35 +32,37 @@ echo 'mongodb-org-tools hold' | sudo dpkg --set-selections
 mkdir -p /db/db_data
 systemctl enable mongod.service
 systemctl start mongod.service
-
 apt-get install -y moreutils
 pip install --upgrade pip
 hash -r
 pip install --upgrade google-cloud-storage
 pip install --upgrade pymongo
 pip install future
-
 mkdir /data; mkdir -p /code/lfadslite; mkdir -p /code/PBT_HP_opt;
-
-gsutil -m cp -r ${BUCKET_DIR}/data/* /data/
-gsutil -m cp -r ${BUCKET_DIR}/code/lfadslite /code/
-gsutil -m cp -r  ${BUCKET_DIR}/code/PBT_HP_opt /code/
-
+gsutil -m cp -r ${CODE_BUCKET}/code/lfadslite /code/
+gsutil -m cp -r  ${CODE_BUCKET}/code/PBT_HP_opt /code/
+# install GCS fuse
+apt-get update
+apt-get install -y gcsfuse
 echo HOSTNAME=${SERVER_NAME} >> /etc/environment
 echo MONGOSERVER=${SERVER_NAME} >> /etc/environment
-
-echo PYTHONPATH=/code/lfadslite:/code/PBT_HP_opt/pbt_opt:$PYTHONPATH >> /etc/environment
-"
-
+echo PYTHONPATH=/code/lfadslite:/code/PBT_HP_opt/pbt_opt:$PYTHONPATH >> /etc/environment"
+echo "Wait for the Server VM to become ready..."
 until gcloud compute ssh ${SERVER_NAME} --command="cat /etc/environment" | grep -q "lfadslite"; do
-  sleep 5
+  sleep 10
 done
-
+echo "Finished Creating the Server VMM."
+echo "Adding a Mongo user"
 gcloud compute ssh ${SERVER_NAME} --zone=us-central1-f --command='sudo mongo admin --host 127.0.0.1:27017 --eval "db.createUser({user: \"pbt_user\", pwd: \"pbt0Pass\", roles: [ { role: \"userAdminAnyDatabase\", db: \"admin\" }]});db.grantRolesToUser(\"pbt_user\", [{ role: \"readWriteAnyDatabase\", db: \"admin\" }]);" && sudo sed -i "/bindIp/d" /etc/mongod.conf && echo "security:
    authorization: enabled
 net: 
    bindIp: 127.0.0.1,`hostname -I`" | sudo tee -a /etc/mongod.conf && sudo systemctl restart mongod.service'
-
+echo "Mounting the bucket"
+gcloud compute ssh ${SERVER_NAME} --zone=us-central1-f --command="mkdir /${BUCKET_NAME} && chmod 777 /${BUCKET_NAME}
+echo '${BUCKET_NAME} /${BUCKET_NAME} gcsfuse rw,auto,user' | sudo tee -a /etc/fstab
+mount /${BUCKET_NAME}
+mkdir /${BUCKET_NAME}/data
+mkdir /${BUCKET_NAME}/runs"
 
 #create TPU nodes
 ind=1
